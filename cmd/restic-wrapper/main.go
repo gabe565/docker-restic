c17c0b4615abd5773f88ed60cab6136f683c5a24
Gabe Cook <git@gabe565.com>
2026-03-22 03:32:48 -0500

    feat: print generated command; add dry-run env

--- cmd/restic-wrapper/main.go
+++ cmd/restic-wrapper/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"io"
 	"log/slog"
 	"os"
 	"path/filepath"
@@ -87,8 +87,38 @@ outer:
 func execRestic(args []string) error {
 	path := "/usr/bin/restic"
 	argv := append([]string{path}, args...)
-	if err := syscall.Exec(path, argv, os.Environ()); err != nil {
-		return fmt.Errorf("exec %s: %w", path, err)
+
+	printCmd(os.Stderr, argv)
+
+	if dryRunStr := os.Getenv("RESTIC_WRAPPER_DRY_RUN"); dryRunStr != "" {
+		if dryRun, _ := strconv.ParseBool(dryRunStr); dryRun {
+			return nil
+		}
 	}
-	return nil
+
+	return syscall.Exec(path, argv, os.Environ())
+}
+
+func printCmd(out io.Writer, args []string) {
+	var n int
+	for _, arg := range args {
+		n += len(arg)
+	}
+
+	var b strings.Builder
+	b.Grow(n + len(args) + 3)
+	b.WriteString("+ ")
+
+	for i, arg := range args {
+		if i != 0 {
+			b.WriteByte(' ')
+		}
+		if strings.Contains(arg, " ") {
+			arg = strconv.Quote(arg)
+		}
+		b.WriteString(arg)
+	}
+
+	b.WriteByte('\n')
+	_, _ = io.WriteString(out, b.String())
 }
